48b4b73fc0b9a00f1e61e34e7e08b3bcbc8cb9f0
CiscoMerchan <franciscoeliasm@yahoo.com>
2023-11-22 22:10:09 +1000

    App.jsx

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,26 @@
-import "./App.css";
-import { Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar/Navbar";
-import Footer from "./components/Footer/Footer";
-import Home from "./pages/Home/Home";
-function App() {
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import {Navbar, Footer} from './components';
+import Home from './pages/Home/Home';
+ 
+import './App.css';
+import './index.css';
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Navbar />
-      </header>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-      <Footer />
-    </div>
+    <Router>
+      <div className='App'>
+        <header className='gradient__bg'>
+          <Navbar />
+        </header>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
