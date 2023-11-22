091097d6880eaefdb296e3da0198786653541004
CiscoMerchan <franciscoeliasm@yahoo.com>
2023-11-22 21:39:37 +1000

    Add Components

---
+++ frontend/src/components/index.js
@@ -0,0 +1,4 @@
+export {default as Cta} from './cta/Cta';
+export {default as feature} from './feature/Feature';
+export {default as Navbar} from './navbar/Navbar';
+export {default as Footer} from './footer/Footer';
