b1ea2a5848f3d6cc528cc4c1b4b9d3b27dcafcc5
CiscoMerchan <franciscoeliasm@yahoo.com>
2023-11-22 22:38:14 +1000

    App.jsx

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,24 +2,23 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import {Navbar, Footer} from './components';
 import Home from './pages/Home/Home';
- 
 import './App.css';
 import './index.css';
 
 const App = () => {
   return (
-    <Router>
-      <div className='App'>
+    <div className='App'>
         <header className='gradient__bg'>
           <Navbar />
         </header>
         <Routes>
           <Route path="/" element={<Home />} />
+           {/* <Route path="/how-it-works" element={<HowItWork />} /> */}
         
         </Routes>
         <Footer />
       </div>
-    </Router>
+    
   );
 };
 
