384d12a47ceac08eee1bf91ebc3224789eb32ade
CiscoMerchan <franciscoeliasm@yahoo.com>
2023-11-23 15:08:43 +1000

    Intro .jsx and .css

--- frontend/src/components/intro/Intro.jsx
+++ frontend/src/components/intro/Intro.jsx
@@ -1,8 +1,17 @@
+/*Intro Component: This component is to be use on each page.
+The component prop are: title, text */
+
 import React from 'react'
 import './Intro.css';
-const Intro = () => {
+const Intro = ({title, text}) => {
   return (
-    <div>Intro</div>
+    <div className='webapp__intro section padding'>
+      <div className='webapp__intro-container'>
+        <h1 className='gradient__text'>{title}</h1>
+        <p>{text}</p>
+       </div>
+    </div>
+    
   )
 }
 
--- frontend/src/pages/HowItWorks/HowItWorks.jsx
+++ frontend/src/pages/HowItWorks/HowItWorks.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import Intro from '../../components/intro/Intro'
 
 const HowItWorks = () => {
   return (
-    <div>HowItWorks</div>
+    <>
+      <Intro
+        title = {`Understanding the Power of Blockchain in Public Works Financing`}
+        text = {`Our platform leverages the power of Polygon and Chainlink to bring transparency and citizen
+        participation to the financing of municipal public works. Let's explore how it all works.`}
+      />
+    </>
   )
 }
 
