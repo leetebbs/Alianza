0a2577f36469f544f198d02a7ec5f4c49fd13c54
CiscoMerchan <franciscoeliasm@yahoo.com>
2023-11-23 15:12:41 +1000

    Intro section

--- frontend/src/pages/HowItWorks/HowItWorks.jsx
+++ frontend/src/pages/HowItWorks/HowItWorks.jsx
@@ -4,11 +4,14 @@ import Intro from '../../components/intro/Intro'
 const HowItWorks = () => {
   return (
     <>
-      <Intro
-        title = {`Understanding the Power of Blockchain in Public Works Financing`}
-        text = {`Our platform leverages the power of Polygon and Chainlink to bring transparency and citizen
-        participation to the financing of municipal public works. Let's explore how it all works.`}
-      />
+      <section>
+        <Intro
+          title = {`Understanding the Power of Blockchain in Public Works Financing`}
+          text = {`Our platform leverages the power of Polygon and Chainlink to bring transparency and citizen
+          participation to the financing of municipal public works. Let's explore how it all works.`}
+        />
+      </section>
+      
     </>
   )
 }
