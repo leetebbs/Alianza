9006122d8da7a6f9ec364f8c027cf03d5e4ee8a4
CiscoMerchan <franciscoeliasm@yahoo.com>
2023-11-24 22:11:14 +1000

    add prop to CTA component to be recycle

--- frontend/src/components/cta/Cta.jsx
+++ frontend/src/components/cta/Cta.jsx
@@ -1,17 +1,23 @@
 // Collect To Action
 import React from 'react';
 import './cta.css'
-
-const Cta = () => {
+/*Props
+cta_title: Title of the banner
+cta_text: Text on the banner
+cta_btn_link:URL to be redirect 
+cta_btn_text:Text of the button
+*/ 
+const Cta = ({cta_title, cta_text, cta_btn_link, cta_btn_text}) => {
   return (
     <div className='webapp__cta section__padding'>
       <div className='webapp__cta-content'>
-        <p>Join us in transforming our city. Purchase tokens, finance public works, track progress,
-and earn rewards. Your participation makes a difference.</p>
-        <h3>Get Involved</h3>
+        <p>{cta_text}</p>
+        <h3>{cta_title}</h3>
       </div>
         <div className='webapp__cta-btn'>
-          <button type='button'>Sign Up Now</button>
+          <button onClick={() => window.location.href = cta_btn_link} type='button'>
+          {cta_btn_text}
+          </button>
         </div>
     </div>
   )
