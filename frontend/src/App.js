777fb3f7fd40a41eb3bdbc30788ada4dd9d6dadd
Tebbo <leetebbs@ymail.com>
2023-11-26 07:49:08 +0000

    add NFT minting addresses to Utils

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar/Navbar';
-import Footer from './components/Footer/Footer'
-import Home from './pages/Home/Home';
-import HowItWorks from './pages/HowItWorks/HowItWorks';
-import './App.css';
-import './index.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
+import Home from "./pages/Home/Home";
+import HowItWorks from "./pages/HowItWorks/HowItWorks";
+import "./App.css";
+import "./index.css";
 
 const App = () => {
   return (
-    <div className='App'>
-        <header className='gradient__bg'>
-          <Navbar />
-        </header>
-        <Routes>
-          <Route path="/" element={<Home />} />
-           <Route path="/how-it-works" element={<HowItWorks />} />
-        
-        </Routes>
-        <Footer />
-      </div>
-    
+    <div className="App">
+      <header className="gradient__bg">
+        <Navbar />
+      </header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/how-it-works" element={<HowItWorks />} />
+      </Routes>
+      <Footer />
+    </div>
   );
 };
 
---
+++ frontend/src/Utils/ContractsAddresses.js
@@ -0,0 +1,8 @@
+export const registrationNFTAddress =
+  "0xe5986Bc8E45F7938bE72E550Aa47FEb211CfC260"; // nft minting contract on the mumbai testnet  function call mintNFT(address)
+export const avaxFujiNFTMintingAddress =
+  "0x759F71Fe9F6366a4bb590Fafd996F41Bb617d34B"; //ccip nft minting contract on the fuji testnet  function call mint(0)
+
+// The contracts will only allow for one NFT per address to be minted.
+// artifacts/registrationABI.js - exports Required ABI for registrationNFT contract
+// artifacts/sourceMinterABI.js - exports Required ABI for avaxFujiNFTMinting contract
