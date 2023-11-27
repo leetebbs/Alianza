e86d492cce3963920c910aa66901622de724fd9c
CiscoMerchan <franciscoeliasm@yahoo.com>
2023-11-27 13:33:14 +1000

    Add PublicWorkSection container

---
+++ frontend/src/containers/PublicWorkSection/PublicWorkSection.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './PublicWorkSection.css'
+const PublicWorkSection = () => {
+  return (
+    <div>PublicWorkSection</div>
+  )
+}
+
+export default PublicWorkSection
