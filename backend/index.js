dab857493de7b964083a5b305b9fcd5175797089
Tebbo <leetebbs@ymail.com>
2023-11-29 20:14:39 +0000

    env.example

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const mongoose = require("mongoose");
 const port = process.env.PORT || 5000;
 const {
   registrationNFTAddress,
