cfba41edfb5da11bce4c0988f971eb59749c5b85
CiscoMerchan <franciscoeliasm@yahoo.com>
2023-12-11 00:28:10 +1000

    Revert "Change in projectcard"
    This reverts commit 33615886d5c745ac0aee24ae832c0cf83d9b58bc.
    
    	modified:   frontend/src/pages/PublicWork/PublicWork.jsx
    	modified:   frontend/src/pages/VotePoject/VotePoject.jsx

--- frontend/src/components/ProjectCard/ProjectCard.jsx
+++ frontend/src/components/ProjectCard/ProjectCard.jsx
@@ -3,15 +3,15 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './ProjectCard.css';
 
-const ProjectCard = ({ id,image, alt_image, title, description, progress, benefit }) => {
+const ProjectCard = ({ work }) => {
   return (
     <div className='webapp__PC-Item'>
-      <Link to={`/public-works/${id}`}>
-        <img src={image} alt={alt_image} />
-        <h3>{title}</h3>
-        <p>Description: {description}</p>
-        <p>Progress: {progress}%</p>
-        <p>Project benefit: {benefit}</p>
+      <Link to={`/public-works/${work.id}`}>
+        <img src={work.image} alt={work.alt_image} />
+        <h3>{work.title}</h3>
+        <p>Description: {work.description}</p>
+        <p>Progress: {work.progress}%</p>
+        <p>Tokens for Financing: {work.tokens}</p>
       </Link>
     </div>
   );
